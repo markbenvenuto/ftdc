82953b9912e74537b4498359a621e96fb7796490
Mark Benvenuto <mark.benvenuto@mongodb.com>
2018-01-16 23:50:02 -0500

    read exact

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 extern crate byteorder;
 
+#[macro_use(bson, doc)]
+extern crate bson;
+
 use std::io;
 // use std::io::prelude::*;
 use std::io::BufReader;
@@ -41,73 +44,79 @@ mod ftdc {
     use std::io::BufReader;
     use std::fs::File;
 use std::io::Read;
+use std::io::Cursor;
+use byteorder::{LittleEndian, ReadBytesExt};
 
-    struct BSONBlockReader {
+
+    pub struct BSONBlockReader {
         reader: BufReader<File>,
-        scratch : Vec<u8>,
-        state : State,
-        buffer : Vec<u8>
     }
 
-    enum RawBSONBlock {
+    pub enum RawBSONBlock {
         Metadata(i32),
         Metrics(i32),
     }
 
     impl BSONBlockReader {
         
-        fn new(file_name : &str) -> BSONBlockReader {
+        pub fn new(file_name : &str) -> BSONBlockReader {
             
             let ff = File::open(file_name).unwrap();
             
             let mut r = BSONBlockReader {
                 reader : BufReader::new(ff),
-                scratch : Vec::with_capacity(4 * 1024),
-                buffer : Vec::with_capacity(4 * 1024),
-                state: State::NeedData,
             };
-
-            r.scratch.resize(4 * 1024, 0);
 
             return r;
         }
-
-        fn read(&mut self) {
-            let size = self.reader.read(&mut self.scratch).unwrap();
-            self.buffer.append(&mut self.scratch);
-        }
     }
 
-    enum State {
-        NeedData,
-        HaveData,
-    }
+    // #[derive(Serialize, Deserialize, Debug)]
+    // pub struct MetadataDoc {
+    //     #[serde(rename = "_id")]  // Use MongoDB's special primary key field name when serializing 
+    //     pub id: Date,
+    //     pub type: i32,
+    //     pub age: i32
+    // }
 
     impl Iterator for BSONBlockReader {
         // add code here
         type Item = RawBSONBlock;
 
         fn next(&mut self) -> Option<RawBSONBlock> {
-            loop {
-            match self.state {
-                State::NeedData => {
-                    self.read();
-                    self.state = State::HaveData;
-                    continue;
-                }
-                State::HaveData => {
-                    // Look for the first 4 bytes
-                    let size : i32 = 4;
+            let mut size_buf:[u8; 4] = [0, 0, 0, 0];            
 
-                    //readBSON
-
-                    // shift buffer down
-                    self.state = State::NeedData;
-
-                    return None;
-                }
+            let result = self.reader.read_exact(&mut size_buf);
+            if result.is_err() {
+                return None;
             }
+
+            // todo: size == 4
+            let mut size_rdr = Cursor::new(size_buf);
+            let size = size_rdr.read_i32::<LittleEndian>().unwrap();
+            println!("size2 {}", size);
+            // // Look for the first 4 bytes
+            // let mut rdr = Cursor::new(self.buffer);
+            // // Note that we use type parameters to indicate which kind of byte order
+            // // we want!
+            // println!("size {}", size);
+
+            let read_size = size as usize;
+            let mut v : Vec<u8> = Vec::with_capacity(read_size);
+            v.resize(read_size, 0);
+            
+            let result = self.reader.read_exact(&mut v[4..]);
+            if result.is_err() {
+                return None;
             }
+            println!("size3 {}", size);
+
+            // let doc = decode_document(&mut Cursor::new(&self.buffer)).unwrap();
+
+            // shift buffer down
+            // self.buffer.po
+
+            return Some(RawBSONBlock::Metadata(123));
         }
     }
 
@@ -119,5 +128,12 @@ fn main() {
     let ftdc_metrics = "/data/db/diagnostic.data/metrics.2017-08-23T15-32-45Z-00000";
 
     decode_file(ftdc_metrics);
+
+
+    let rdr = ftdc::BSONBlockReader::new(ftdc_metrics);
+
+    for item in rdr {
+        println!("found ");
+    }
 
 }
